23a1937d63d1d1bdee1248c7087331465dfc4a4c
GuiLeoni14 <guileone167@gmail.com>
2021-11-27 19:56:57 -0200

    renderizacao condicional

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import Eventos from './components/Eventos';
 import Form from './components/Form';
 import Condicional from './components/Condicional';
+import OutraLista from './components/OutraLista';
 
 function App() {
+
+   const meusItens = ['React', 'Vue', 'Angular']
+   
    return (
       <div className="App">
-         <h1>Renderização condicional</h1>
-         <Condicional/>
+         <h1>Renderização de Listar</h1>
+         <OutraLista itens={[]}/>
+         <OutraLista itens={meusItens}/>
       </div>
    );
 }
---
+++ src/components/OutraLista.js
@@ -0,0 +1,16 @@
+function OutraLista({itens}){
+    return(
+        <>
+            <h3>Listas de coisas boas</h3>
+            {itens.lenght > 0 ? (
+                itens.map((item, index) => (
+                    <p key={index}>{item}</p>
+                ))) : (
+                    <p>Nao existe itens</p>
+                )
+            }
+        </>
+    )
+}
+
+export default OutraLista;
