f9be0c82aafc174f1c324525ebbf6f36ff754a3a
GuiLeoni14 <guileone167@gmail.com>
2022-01-25 20:16:09 -0200

    adicionando react-icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,13 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
 function Footer () {
     return (
         <footer>
             <h1>Rodapé</h1>
+            <ul>
+                <li><FaFacebook/></li>
+                <li><FaInstagram/></li>
+                <li><FaLinkedin/></li>
+            </ul>
         </footer>
     )
 }
